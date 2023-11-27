c8f80e6b02a8cda06aa525e6935519b98d17ef91
Rysth <johnpalacios.t@gmail.com>
2023-11-27 14:45:04 -0500

    Setup app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Greeting from './components/Greeting/Greeting';
+import store from './redux/store';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Greeting />,
+  },
+]);
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Provider store={store}>
+      <main class="max-w-screen-xl mx-auto px-4 py-20">
+        <RouterProvider router={router} />
+      </main>
+    </Provider>
+  );
 }
 
 export default App;
